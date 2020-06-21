8d330315dc372b7a1076a234615ab9a4fa96cdd0
JimmyAzar <54740893+JimmyAzar@users.noreply.github.com>
2020-06-21 11:38:26 +0300

    Tune RDOCC hyperparameters

---
+++ tune_rdc.R
@@ -0,0 +1,44 @@
+tune_rdc <- function(a_train,a_val,fracrej,k,h,verbose=TRUE){
+  #a_train: labeled training set as data frame (0=inlier, 1=outlier)
+  #a_val: labeled validation set as data frame
+  #fracrej: range of valuea for reject fraction in [0,1]
+  #k: range of values for number of neighbors k
+  #h: range of values for kernel parameter h
+  
+  err <- array(NA,dim=c(length(fracrej),length(h),length(k)))
+  
+  Yl <- a_train[a_train[,ncol(a_train)]==0,]
+  for (j in seq_along(fracrej)){
+    if (verbose) print(paste("counter:...",length(fracrej)-j))
+    for (l in seq_along(h)){
+      out <- rdocc_scalable(as.matrix(Yl), as.matrix(a_val), h=h[l], k=k, 
+                            fracrej = fracrej[j], verbose=FALSE)
+      for (m in seq_along(k)){
+        pred <- out[[m]]
+
+        FP <- sum(pred==1 & a_val[,ncol(a_val)]==0)
+        TN <- sum(pred==0 & a_val[,ncol(a_val)]==0)
+        FN <- sum(pred==0 & a_val[,ncol(a_val)]==1)
+        TP <- sum(pred==1 & a_val[,ncol(a_val)]==1)
+          
+        error <- (FP/(FP+TN)) + (FN/(FN+TP)) #= FPR + FNR
+        error = error/2
+        err[j,l,m] <- error
+      }
+    }
+  }
+
+  ind_opt <- which(err==min(err), arr.ind = TRUE)
+  if (is.array(ind_opt)){
+    fracrej_opt <- fracrej[ind_opt[1,1]]
+    h_opt <- h[ind_opt[1,2]]
+    k_opt <- k[ind_opt[1,3]]
+  }
+  else {
+    fracrej_opt <- fracrej[ind_opt[1]]
+    h_opt <- h[ind_opt[2]]
+    k_opt <- k[ind_opt[3]]
+  }
+  l = list(fracrej_opt=fracrej_opt,k_opt=k_opt,h_opt=h_opt)
+  return(l)
+}
